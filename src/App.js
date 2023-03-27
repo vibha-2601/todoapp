24c122b499a082cb0e092af2f90fda172917609d
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-27 11:01:38 +0530

    create deleteTodo functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import { useState } from 'react';
-import './App.css';
-import TodoInput from './components/TodoInput/TodoInput'
-import TodoList from './components/TodoList/TodoList';
+import { useState } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput/TodoInput";
+import TodoList from "./components/TodoList/TodoList";
 function App() {
-
-   // data
+  // data
   const [todoList, setTodoList] = useState([]);
 
   // add functionality
   let addToDo = (inputText) => {
+    if(inputText !== '')
     setTodoList([...todoList, inputText]);
-    
   };
+
+  // delete functionality
+  const deleteToDo = (key) => {
+    let newTodoList = [...todoList];
+    newTodoList.splice(key, 1);
+    setTodoList([...newTodoList]);
+  }
+
   return (
     <div className="App">
-      <TodoInput addToDo={addToDo}/>
+      <TodoInput addToDo={addToDo} />
       <div style={{ fontSize: 25, fontWeight: "bold", textAlign: "center" }}>
         TODO
       </div>
       <hr style={{ width: "80%" }}></hr>
-<TodoList />
+      {todoList.map((listItem, index) => (
+        <TodoList key={index} index={index} item={listItem} deleteItem={deleteToDo}/>
+      ))}
     </div>
   );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,25 +1,22 @@
-import { Grid, IconButton, TextField } from "@mui/material";
+import {  IconButton, TextField } from "@mui/material";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import React from "react";
 
-const TodoList = ({item}) => {
+const TodoList = ({ item, index, deleteItem }) => {
   return (
     <div>
-      
-        <TextField
-          style={{ width: "60%" }}
-          label="Name"
-          placeholder="Item"
-          variant="outlined"
-          value={item}
-        />
-        <IconButton color="secondary">
-          <DeleteOutlineIcon />
-        </IconButton>
-
-      
+      <TextField
+        style={{ width: "60%", paddingBottom: '10px' }}
+        label="Name"
+        placeholder="Item"
+        variant="outlined"
+        value={item}
+      />
+      <IconButton color="secondary" onClick={(e) => deleteItem(index)}>
+        <DeleteOutlineIcon />
+      </IconButton>
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
